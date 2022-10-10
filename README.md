adf22c5871904f7417f6687010d3202c3af533b5
Rasmus Bondesson <raek@raek.se>
2022-10-10 21:13:31 +0200

    Working version

---
+++ README.md
@@ -0,0 +1,94 @@
+# pypi-multidl
+
+This tool was made to make it easier to download Python packages from one
+computer and install them on other computers that are not connected to the
+internet (and may run different OSes). This project is not officially endorsed
+by anyone (for example, the maintainers of PyPI or `pip`).
+
+Its input is a list of dependency specifications (package name and versions),
+and the output is a destination directory populated with all the sdists and
+wheels from PyPI that matches the specifications. This directory can then be
+used offline with `pip` using the `-f/--find-links` option as a lightweight PyPI
+mirror.
+
+This tool does no dependency resolution. You are expected to use `pip-compile`
+from the `pip-tools` package (or an equivalent tool) to find all the
+dependencies of the dependencies, and pinning down all the exact version.
+`pip-compile` can be used to process a `setup.py`/`setup.cfg`/`pyproject.toml`
+file into a `requirements.txt` to be used by `pypi-multidl`.
+
+## Example Usage: Download Dependencies for a Project
+
+Assume you have a Python project you want to be able to install offline, and
+that its dependencies aren't pinned to specific version. Then begin by compiling
+the list of transitive pinned dependencies:
+
+    cd some-project
+	pip-compile setup.py # ...or setup.cf, or pyproject.toml
+
+Now you have a `requriements.txt` file. Continue to download all the
+dependencies to the `deps/` directory:
+
+    pypi-multidl -r requirements.txt -d deps/
+
+The project can then be installed offline like this:
+
+    pip install --no-index --find-links deps/ .
+
+## Example Usage: List all Distribution Packages Avaiable for a Project
+
+The `pypi-multidl` tool can also be given dependency specifications directly on
+the command line:
+
+    pypi-multidl --dry-run 'appdirs' 'sphinx >=4.0.0, <5.0.0'
+
+## Limitations
+
+The approach used by pypi-multidl does not work in the general case. The
+dependency resolution step `pip-compile` is actually platform-specific. A
+package can have different dependencies on different platforms. For example,
+Sphinx depends on `colorama`, but only on Windows. What you get from
+`pip-compile` is the set of dependencies specific for the plaform that
+`pip-compile` was run on. If no packages have platform-specific dependencies,
+then everything should work. Currently, `pypi-multidl` does not make any attempt
+to validate that the `requirements.txt` is platform-independent.
+
+This tool currently only supports the PEP 691 JSON API via the simple endpoint.
+
+Yanked files are never downloaded, not even when pinned using `==` (see PEP
+592).
+
+Requires-python and other metadata is not downloaded.
+
+This tool only produces a flat directory of files, not a "simple" style
+directory tree to be served by a web server.
+
+## Comparison to other tools
+
+The `pip download` command only supports downloading packages for one system at
+a time. It also only downloads an sdist or a wheel, but never both.
+
+The `pip-downloader` tool solves the same problem as `pypi-multi`, but is
+implemented in a different way. It performs dependency resolution using `pip`
+and uses its internal API. It needs to continually be updated whenever `pip`
+changes in order not to break.
+
+## Usage with a Local Cache
+
+When experimenting with downloading from PyPI it is recommended to setup a local
+PyPI cache. The `devpi` tool is easy to get started with:
+
+    pip install devpi
+	devpi-init
+	devpi-server  # Runs an index on http://localhost:3141/root/pypi/+simple/
+
+Leave that running in a terminal (or add it to your init system) and add this to
+your pip config:
+
+    [global]
+    index-url = http://localhost:3141/root/pypi/+simple/
+
+`pip`, `pip-compile` and `pypi-multidl` will then automatically use the cache.
+Note that you also need to pass `--no-emit-options` to `pip-compile` so that it
+doen't add an `--index-url` line to the `requirements.txt` (which `pypi-multidl`
+doesn't understand).
---
+++ pypi_multidl.py
@@ -0,0 +1,167 @@
+"""A tool to download Python packages for later offline installation on many platforms"""
+
+from argparse import ArgumentParser
+from collections import namedtuple
+import hashlib
+from pathlib import Path
+import subprocess
+import sys
+from urllib.parse import urljoin
+
+from packaging.requirements import Requirement
+import packaging.utils as u
+import requests
+
+
+__version__ = "1.0.0alpha1"
+
+DEFAULT_INDEX_URL = "https://pypi.org/simple/"
+
+
+Download = namedtuple("Download", "filename, url, hash_algo, hash_digest")
+
+
+def main():
+    args = parse_args()
+    requirements = collect_requirements(args)
+    index_url = args.index_url or find_index_url()
+    dest_dir = Path(args.dest_dir)
+    dest_dir.mkdir(parents=True, exist_ok=True)
+    downloads = find_downloads(requirements, index_url, dest_dir)
+    if args.dry_run:
+        for download in downloads:
+            print(download.filename)
+    else:
+        for download in downloads:
+            download_file(download, dest_dir)
+
+
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument("-V", "--version", action="store_true",
+                        help="Display version and quit.")
+    parser.add_argument("-r", "--requirement", metavar="<file>",
+                        action="append", dest="reqfiles", default=[],
+                        help="File with requirements (\"requirements.txt\")")
+    parser.add_argument("requirements", metavar="<requirement>", nargs="*",
+                        help="Dependency requirement (eg. foobar==1.2.3)")
+    parser.add_argument("-d", "--dest-dir", default=".",
+                        help="Which directory to download files to "
+                        "(default: current directory)")
+    parser.add_argument("-i", "--index-url",
+                        help="Which package index to use. "
+                        "Defaults \"pip config get global.index-url\" if set, "
+                        "or PyPI otherwise.")
+    parser.add_argument("-n", "--dry-run", action="store_true",
+                        help="Down download files, just list them.")
+    args = parser.parse_args()
+    if args.version:
+        print(f"pypi-multidl {__version__}")
+        sys.exit(0)
+    return args
+
+
+def collect_requirements(args):
+    requirements = []
+    for reqfile in args.reqfiles:
+        with open(reqfile, "r", encoding="utf-8") as f:
+            for line in f:
+                line = line.strip()
+                if not line or line.startswith("#"):
+                    continue
+                requirements.append(Requirement(line))
+    for req in args.requirements:
+        requirements.append(Requirement(req))
+    return requirements
+
+
+def find_index_url():
+    cmdline = [sys.executable, "-m", "pip",
+               "config", "get", "global.index-url"]
+    comp = subprocess.run(cmdline, capture_output=True, encoding="utf-8")
+    if comp.returncode == 0:
+        index_url = comp.stdout.strip()
+    else:
+        index_url = DEFAULT_INDEX_URL
+    if not index_url.endswith("/"):
+        index_url += "/"
+    return index_url
+
+
+def find_downloads(requirements, index_url, dest_dir):
+    for req in requirements:
+        yield from find_project_downloads(req.name, req.specifier,
+                                          index_url, dest_dir)
+
+
+def find_project_downloads(project_name, specifier_set, index_url, dest_dir):
+    project_url = index_url + project_name + "/"
+    headers = {"Accept": "application/vnd.pypi.simple.v1+json"}
+    r = requests.get(project_url, headers=headers)
+    project_json = r.json()
+    for file_json in project_json["files"]:
+        filename = file_json["filename"]
+        if filename in [".", ".."] or "/" in filename:
+            # Dangerous filenames that should never be used.
+            continue
+        if file_json.get("yanked", False):
+            # We should only mirror yanked files if we also mirror the "yanked"
+            # attribute. See: https://peps.python.org/pep-0592/#mirrors
+            continue
+        version = version_from_filename(filename)
+        if not version:
+            # Might be a Windows installer or some other lesser used format
+            continue
+        if version not in specifier_set:
+            continue
+        url = urljoin(project_url, file_json["url"])
+        # Pick first hash we support (if there is any)
+        for algo, digest in file_json["hashes"].items():
+            algo = algo.lower()
+            if algo in hashlib.algorithms_available:
+                hash_algo = algo
+                hash_digest = digest.lower()
+                break
+        else:
+            hash_algo = None
+            hash_digest = None
+        yield Download(filename, url, hash_algo, hash_digest)
+
+
+def version_from_filename(filename):
+    try:
+        return u.parse_wheel_filename(filename)[1]
+    except u.InvalidWheelFilename:
+        pass
+    except Exception:
+        return None
+    try:
+        return u.parse_sdist_filename(filename)[1]
+    except u.InvalidSdistFilename:
+        pass
+    except Exception:
+        # packaging.version.InvalidVersion will be raised if the sdist filename
+        # has more than one hyphen (for example when the version does not follow
+        # PEP 440)
+        return None
+    # Known extra cases: Windows installers
+    # * foobar-1.2.3.win32.exe
+    # * foobar-1.2.3.win32-pyX.Y.exe
+    return None
+
+
+def download_file(download, dest_dir):
+    filename, url, hash_algo, hash_digest = download
+    with (dest_dir / filename).open("wb") as f:
+        print(filename)
+        if hash_algo:
+            h = hashlib.new(hash_algo)
+        r = requests.get(url, stream=True)
+        for chunk in r.iter_content(chunk_size=4096):
+            f.write(chunk)
+            if hash_algo:
+                h.update(chunk)
+        if hash_algo:
+            if h.hexdigest() != hash_digest:
+                msg = f"{hash_algo} hash for {filename} did not match!"
+                raise Exception(msg)
